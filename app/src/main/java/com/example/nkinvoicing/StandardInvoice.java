897eb3e8b9ba3c0ba1e0137b7735698df3b42262
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2021-01-21 19:20:30 +0000

    invoice table added
    I was able to dynamically generate a table with data (hardcoded for now).
    To do: make the screen scrollable so that the table can extend. Edit the colours and the size of the table.

--- app/src/main/java/com/example/nkinvoicing/StandardInvoice.java
+++ app/src/main/java/com/example/nkinvoicing/StandardInvoice.java
@@ -1,6 +1,11 @@
 package com.example.nkinvoicing;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +14,54 @@ public class StandardInvoice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.standard_invoice);
+        init();
+    }
+    public void init() {
+        /*
+        int tableId = getResources().getIdentifier(standard_invoice_table, "id", getPackageName());*/
+        TableLayout stk = (TableLayout) findViewById(R.id.standard_invoice_table);
+        TableRow tbrow0 = new TableRow(this);
+        TextView tv0 = new TextView(this);
+        tv0.setText(" Sl.No ");
+        tv0.setTextColor(Color.BLACK);
+        tbrow0.addView(tv0);
+        TextView tv1 = new TextView(this);
+        tv1.setText(" Product ");
+        tv1.setTextColor(Color.BLACK);
+        tbrow0.addView(tv1);
+        TextView tv2 = new TextView(this);
+        tv2.setText(" Unit Price ");
+        tv2.setTextColor(Color.BLACK);
+        tbrow0.addView(tv2);
+        TextView tv3 = new TextView(this);
+        tv3.setText(" Stock Remaining ");
+        tv3.setTextColor(Color.BLACK);
+        tbrow0.addView(tv3);
+        stk.addView(tbrow0);
+        for (int i = 0; i < 25; i++) {
+            TableRow tbrow = new TableRow(this);
+            TextView t1v = new TextView(this);
+            t1v.setText("" + i);
+            t1v.setTextColor(Color.BLACK);
+            t1v.setGravity(Gravity.CENTER);
+            tbrow.addView(t1v);
+            TextView t2v = new TextView(this);
+            t2v.setText("Product " + i);
+            t2v.setTextColor(Color.BLACK);
+            t2v.setGravity(Gravity.CENTER);
+            tbrow.addView(t2v);
+            TextView t3v = new TextView(this);
+            t3v.setText("Rs." + i);
+            t3v.setTextColor(Color.BLACK);
+            t3v.setGravity(Gravity.CENTER);
+            tbrow.addView(t3v);
+            TextView t4v = new TextView(this);
+            t4v.setText("" + i * 15 / 32 * 10);
+            t4v.setTextColor(Color.BLACK);
+            t4v.setGravity(Gravity.CENTER);
+            tbrow.addView(t4v);
+            stk.addView(tbrow);
+        }
+
     }
 }
