758aefb5bf47ada627b8bf5b1e44bf7d53af3fc8
mrKrastev <56829655+mrKrastev@users.noreply.github.com>
2021-01-29 15:03:23 +0000

    minor error fix

--- app/src/main/java/com/example/nkinvoicing/InvoiceCreator.java
+++ app/src/main/java/com/example/nkinvoicing/InvoiceCreator.java
@@ -112,7 +112,7 @@ public class InvoiceCreator extends AppCompatActivity implements DatePickerDialo
         //Create invoice Object
         InvoiceData invObj = new InvoiceData(contacts,
                 tableItems,
-                String.valueOf(issueDate.getText()),
+                String.valueOf(invoiceNo.getText()),
                 String.valueOf(issueDate.getText()),
                 String.valueOf(dueDate.getText()));
 
